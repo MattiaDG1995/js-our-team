0c094bb8013cd0683661d6215fadf77911c1658b
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-11-04 14:27:38 +0100

    card

--- main.js
+++ main.js
@@ -30,3 +30,20 @@ const team = [
       image: 'barbara-ramos-graphic-designer.jpg',
     },
   ];
+
+  let percorso ='assets/img/';
+
+  for(let i=0; i<team.length; i++){
+
+    let elementoTeam = team[i]
+    console.log(elementoTeam.name)
+
+    document.getElementById('card').innerHTML += `
+    <div class="card" style="width: 18rem;">
+        <img src= "${percorso + elementoTeam.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${elementoTeam.name}</h5>
+        <p class="card-text">${elementoTeam.role}</p>
+    </div>
+    </div>`
+  }
